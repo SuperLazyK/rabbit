32b8de9c5a8dbe73df3fed2240bfbc9a10c12409
hoge <hoge>
2022-08-09 07:52:16 +0900

    bk

--- env.py
+++ env.py
@@ -7,186 +7,230 @@ from os import path
 import time
 import control
 
-def calc_dstate(state, u):
-    #if y > 0:
-    #else:
+IDX_x    = 0
+IDX_y    = 1
+IDX_th0  = 2
+IDX_th1  = 3
+IDX_th2  = 4
+IDX_dx   = 5
+IDX_dy   = 6
+IDX_dth0 = 7
+IDX_dth1 = 8
+IDX_dth2 = 9
+
+def rhs_foot_contact(t, s, u, params):
+    g  = params.get('g',     9.8)
+    m0 = params.get('m0',    1.)
+    m1 = params.get('m1',    1.)
+    m2 = params.get('m2',    2.)
+    l0 = params.get('l0',    1.)
+    l1 = params.get('l1',    1.)
+    l2 = params.get('l2',    1.)
     pass
 
 
-def normalize_state(state):
-    #if y < 0:
-    #   y = 0
+def rhs_foot_in_the_air(t, s, u, params):
+    g  = params.get('g',     9.8)
+    m0 = params.get('m0',    1.)
+    m1 = params.get('m1',    1.)
+    m2 = params.get('m2',    2.)
+    l0 = params.get('l0',    1.)
+    l1 = params.get('l1',    1.)
+    l2 = params.get('l2',    1.)
     pass
 
-class RabbitState():
-    def __init__(self, randomize=False):
-        #const
-        self.dt=.05
-        self.g = g
-        self.m0 = 1.
-        self.m1 = 1.
-        self.m2 = 2.
-        self.l0 = 1.
-        self.l1 = 1.
-        self.l2 = 1.
 
-        #constraint
-        self.max_torque=2.
-
-        self.max_x0   = 10.
-        self.max_y0   = 10.
-        self.max_th0  = np.pi
-        self.max_th1  = np.pi
-        self.max_th2  = np.pi
-        self.max_dx0  = 5.
-        self.max_dy0  = 5.
-        self.max_dth0 = 8.
-        self.max_dth1 = 8.
-        self.max_dth2 = 8.
-        self.min_x0   = -10.
-        self.min_y0   = -10.
-        self.min_th0  = 0
-        self.min_th1  = 0
-        self.min_th2  = -np.pi
-        self.min_dx0  = -5.
-        self.min_dy0  = -5.
-        self.min_dth0 = -8.
-        self.min_dth1 = -8.
-        self.min_dth2 = -8.
-
-        #state
-        self.x0   = 0.
-        self.y0   = 0.
-        self.th0  = np.pi/2
-        self.th1  = np.pi/2
-        self.th2  = -np.pi/3
-        self.dx0  = 0.
-        self.dy0  = 0.
-        self.dth0 = 0.
-        self.dth1 = 0.
-        self.dth2 = 0.
-
-        if randomize:
-            self.th0 = self.np_random.uniform(low=-np.pi/4, high=self.np.pi/4)
-            self.th1 = self.np_random.uniform(low=-np.pi/4, high=self.np.pi/4)
-            self.th2 = self.np_random.uniform(low=-np.pi/4, high=self.np.pi/4)
-
-    def obs(self, u):
-        return np.array([ self.th0
-                        , self.th1
-                        , self.th2
-                        , self.dth0
-                        , self.dth1
-                        , self.dth2
-        ], dtype=np.float32)
-
-        self.observation_space = spaces.Box(low=ob_low, high=ob_high, dtype=np.float32)
-        max_action = np.array([self.max_torque, self.max_torque])
-        self.action_space = spaces.Box(low=-max_action, high=max_action, dtype=np.float32)
-
-    def ob_high(self):
-        return np.array([ np.pi #th0
-                        , np.pi #th1
-                        , np.pi #th2
-                        , self.max_rot_speed #dth0
-                        , self.max_rot_speed #dth1
-                        , self.max_rot_speed #dth2
-        ], dtype=np.float32)
-
-    def ob_low(self):
-        return np.array([ 0      #th0
-                        , 0      #th1
-                        , -np.pi #th2
-                        , self.max_rot_speed #dth0
-                        , self.max_rot_speed #dth1
-                        , self.max_rot_speed #dth2
-        ], dtype=np.float32)
+def rhs(t, s, u, params):
+    assert s.y >= 0, ""
+    if s[IDX_y] == 0:
+        return rhs_foot_contact(t, s, u, params)
+    else:
+        return rhs_foot_in_the_air(t, s, u, params)
 
 
-class RabbitEnv(gym.Env):
-    metadata = {
-        'render.modes' : ['human', 'rgb_array'],
-        'video.frames_per_second' : 30
-    }
-
-    def __init__(self):
-        self.state = RabbitEnv(False)
-        self.viewer = None
-        self.frame_no = 0
-
-        self.observation_space = spaces.Box(low=ob_low, high=ob_high, dtype=np.float32)
-        max_action = np.array([self.max_torque, self.max_torque])
-        self.action_space = spaces.Box(low=-max_action, high=max_action, dtype=np.float32)
-
-        self.seed()
+def obs(t, s, u, params):
+    pass
 
 
-    def seed(self, seed=None):
-        self.np_random, seed = seeding.np_random(seed)
-        return [seed]
+model = ct.NonlinearIOSystem(rhs, obs
+        , inputs=('tau1', 'tau2')
+        , outputs=('th0', 'th1', 'th2', 'dth0', 'dth1', 'dth2', 'fy')
+        , states=('x', 'y', 'th0', 'th1', 'th2', 'dx', 'dy', 'dth0', 'dth1', 'dth2')
+        , name='rabit')
 
 
-    def step(self, u):
-        th, thdot = self.state # th := theta
+def step(x, u, p, te=1):
+    ts=0
+    p0 = {'F0':p}
+    u = np.repeat(np.array(u).reshape(Nu,1), te-ts+1, axis=1)
+    T = np.array(range(ts,te+1))
+    t, x = ct.input_output_response(io_ex1_11, T, U=u, X0=x, params=p0)
+    return x[:,-1]
 
-        g = self.g
-        m = self.m
-        l = self.l
-        dt = self.dt
+#class RabbitState():
+#    def __init__(self, randomize=False):
+#        #const
+#        self.dt=.05
+#
+#        #constraint
+#        self.max_torque=2.
+#
+#        self.max_x0   = 10.
+#        self.max_y0   = 10.
+#        self.max_th0  = np.pi
+#        self.max_th1  = np.pi
+#        self.max_th2  = np.pi
+#        self.max_dx0  = 5.
+#        self.max_dy0  = 5.
+#        self.max_dth0 = 8.
+#        self.max_dth1 = 8.
+#        self.max_dth2 = 8.
+#        self.min_x0   = -10.
+#        self.min_y0   = -10.
+#        self.min_th0  = 0
+#        self.min_th1  = 0
+#        self.min_th2  = -np.pi
+#        self.min_dx0  = -5.
+#        self.min_dy0  = -5.
+#        self.min_dth0 = -8.
+#        self.min_dth1 = -8.
+#        self.min_dth2 = -8.
+#
+#        #state
+#        self.x0   = 0.
+#        self.y0   = 0.
+#        self.th0  = np.pi/2
+#        self.th1  = np.pi/2
+#        self.th2  = -np.pi/3
+#        self.dx0  = 0.
+#        self.dy0  = 0.
+#        self.dth0 = 0.
+#        self.dth1 = 0.
+#        self.dth2 = 0.
+#
+#        if randomize:
+#            self.th0 = self.np_random.uniform(low=-np.pi/4, high=self.np.pi/4)
+#            self.th1 = self.np_random.uniform(low=-np.pi/4, high=self.np.pi/4)
+#            self.th2 = self.np_random.uniform(low=-np.pi/4, high=self.np.pi/4)
+#
+#        self.model = ct.NonlinearIOSystem(self.rhs
+#                , None
+#                , inputs=('tau1', 'tau2')
+#                , outputs=('c', 'T', 'h')
+#                , states=('c', 'T', 'h')
+#                , name='rabbit')
+#
+#    def obs_space(self):
+#        ob_low = np.array([ self.min_th0
+#                          , self.min_th1
+#                          , self.min_th2
+#                          , self.min_dth0
+#                          , self.min_dth1
+#                          , self.min_dth2
+#                          , 0  # fy
+#        ], dtype=np.float32)
+#
+#        ob_high = np.array([ self.max_th0
+#                           , self.max_th1
+#                           , self.max_th2
+#                           , self.max_dth0
+#                           , self.max_dth1
+#                           , self.max_dth2
+#                           , 10
+#                    ], dtype=np.float32)
+#
+#        return spaces.Box(low=ob_low, high=ob_high, dtype=np.float32)
+#
+#
+#    def act_space(self):
+#        max_action = np.array([self.max_torque, self.max_torque])
+#        return spaces.Box(low=-max_action, high=max_action, dtype=np.float32)
+#
+#
+#    def step(self, u):
+#        if self.y0 > 0:
+#            self.step_in_
+#
+#        th, thdot = self.state # th := theta
+#
+#        g = self.g
+#        m = self.m
+#        l = self.l
+#        dt = self.dt
+#
+#        u = np.clip(u, -self.max_torque, self.max_torque)
+#        dstate = calc_dstate(state, u)
+#        state = state + dstate * dt
+#
+#        newthdot = thdot + (-3*g/(2*l) * np.sin(th + np.pi) + 3./(m*l**2)*u) * dt
+#        newth = normalize_state(th + newthdot*dt)
 
-        u = np.clip(u, -self.max_torque, self.max_torque)
-
-        costs = angle_normalize(th)**2 + .1*thdot**2 + .001*(u**2)
-
-        dstate = calc_dstate(state, u)
-        state = state + dstate * dt
-
-        newthdot = thdot + (-3*g/(2*l) * np.sin(th + np.pi) + 3./(m*l**2)*u) * dt
-        newth = normalize_state(th + newthdot*dt)
-
-        self.state = newth
-        self.frame_no = self.frame_no + 1
-
-        #print((self.frame_no, angle_normalize(th), thdot))
-
-        theta, thetadot = self.state
-        obs = np.array([np.cos(theta), np.sin(theta), thetadot])
-
-        return self._get_obs(), -costs, False, {}
 
 
-    def reset(self):
-        self.state = RabbitEnv(False)
-        self.frame_no = 0
-        return self.state.obs(u = np.array([0, 0]))
-
-
-    def render(self, mode='human'):
-
-        if self.viewer is None:
-            from gym.envs.classic_control import rendering
-            self.viewer = rendering.Viewer(500,500)
-            self.viewer.set_bounds(-2.2,2.2,-2.2,2.2)
-            rod = rendering.make_capsule(1, .2)
-            rod.set_color(.8, .3, .3)
-            self.pole_transform = rendering.Transform()
-            rod.add_attr(self.pole_transform)
-            self.viewer.add_geom(rod)
-            axle = rendering.make_circle(.05)
-            axle.set_color(0,0,0)
-            self.viewer.add_geom(axle)
-
-        self.pole_transform.set_rotation(self.state[0] + np.pi/2)
-        #if self.frame_no < 3:
-        #    time.sleep(1)
-        #time.sleep(0.1)
-
-        return self.viewer.render(return_rgb_array = mode=='rgb_array')
-
-    def close(self):
-        if self.viewer:
-            self.viewer.close()
-            self.viewer = None
-
-def angle_normalize(x):
-    return (((x+np.pi) % (2*np.pi)) - np.pi)
+#class RabbitEnv(gym.Env):
+#    metadata = {
+#        'render.modes' : ['human', 'rgb_array'],
+#        'video.frames_per_second' : 30
+#    }
+#
+#    def __init__(self):
+#        self.state = RabbitEnv(False)
+#        self.viewer = None
+#        self.frame_no = 0
+#        self.observation_space = self.state.obs_space()
+#        self.action_space = self.state.act_space()
+#        self.seed()
+#
+#
+#    def seed(self, seed=None):
+#        self.np_random, seed = seeding.np_random(seed)
+#        return [seed]
+#
+#    def reward(self):
+#        #TODO
+#        return 0
+#
+#    def check_done(self):
+#        #TODO
+#        return False
+#
+#    def step(self, u):
+#        obs = self.state.step(u)
+#        self.frame_no = self.frame_no + 1
+#        return obs, self.reward(), self.check_done(), {}
+#
+#    def reset(self):
+#        self.state = RabbitEnv(False)
+#        self.frame_no = 0
+#        obs = self.state.step(u = np.array([0, 0]))
+#        return obs
+#
+#
+#    def render(self, mode='human'):
+#        #TODO
+#
+#        if self.viewer is None:
+#            from gym.envs.classic_control import rendering
+#            self.viewer = rendering.Viewer(500,500)
+#            self.viewer.set_bounds(-2.2,2.2,-2.2,2.2)
+#            rod = rendering.make_capsule(1, .2)
+#            rod.set_color(.8, .3, .3)
+#            self.pole_transform = rendering.Transform()
+#            rod.add_attr(self.pole_transform)
+#            self.viewer.add_geom(rod)
+#            axle = rendering.make_circle(.05)
+#            axle.set_color(0,0,0)
+#            self.viewer.add_geom(axle)
+#
+#        self.pole_transform.set_rotation(self.state[0] + np.pi/2)
+#        #if self.frame_no < 3:
+#        #    time.sleep(1)
+#        #time.sleep(0.1)
+#
+#        return self.viewer.render(return_rgb_array = mode=='rgb_array')
+#
+#    def close(self):
+#        if self.viewer:
+#            self.viewer.close()
+#            self.viewer = None
+#
